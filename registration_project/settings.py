c563f15d2138152fe17c841d85d75e68cd97a71b
HREngCode <ccrsailors@gmail.com>
2023-03-06 18:08:35 -0600

    Removed Secret Key And Database Information

--- registration_project/settings.py
+++ registration_project/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d5dseb*l_$6!uw^y0zdzdv8&lazwn#l()&45@nojxana%76+z-'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -72,13 +69,6 @@ WSGI_APPLICATION = 'registration_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
